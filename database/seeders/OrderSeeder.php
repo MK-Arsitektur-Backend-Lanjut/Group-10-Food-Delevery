8bb27896102dc9f446a53a447e1114ab62663845
divalunettaa <divalunetta@gmail.com>
2026-04-29 18:49:12 +0700

    Update OrderSeeder.php
    update OrderSeeeder.php

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,21 +2,34 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Order;
+use App\Models\OrderStatusLog;
 
 class OrderSeeder extends Seeder
 {
-  public function run()
-  {
-    for ($i = 0; $i < 10000; $i++) {
-        Order::create([
-            'user_id' => rand(1, 100),
-            'restaurant_id' => rand(1, 10),
-            'status' => 'DIPESAN',
-            'total_price' => rand(10000, 50000)
-        ]);
-      }
-    } 
+    public function run(): void
+    {
+        for ($i = 0; $i < 10000; $i++) {
+
+            $order = Order::create([
+                'user_id' => rand(1, 100),
+                'restaurant_id' => rand(1, 10),
+                'status' => 'SELESAI',
+                'total_price' => rand(10000, 50000),
+            ]);
+
+            // histori status
+            $statuses = ['DIPESAN', 'DIMASAK', 'DIANTAR', 'SELESAI'];
+
+            foreach ($statuses as $status) {
+                OrderStatusLog::create([
+                    'order_id' => $order->id,
+                    'status' => $status,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
+    }
 }
